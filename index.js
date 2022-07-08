435bb76d89f3ab1bdf7111ad7e81bb5cb4d7999a
Misael Calvillo Mancilla <misa@homely.mx>
2022-07-07 20:00:59 -0500

    3. Crea un servidor de express

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const PORT = 3000
 
+app.get('/', (req, res) => {
+  res.send('Hola desde nuestra app 🚀')
+})
+
+app.listen(PORT, () => {
+  console.log(`El servidor esta corriendo exitosamente en http://localhost:${PORT} ✅`)
+})
